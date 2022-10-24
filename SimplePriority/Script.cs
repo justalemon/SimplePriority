9cce6363082f19729d2ee4c8a5399ad044bbc65c
Hannele Ruiz <justlemoncl@gmail.com>
2022-10-23 22:15:24 -0300

    Added simple script

---
+++ SimplePriority/Script.cs
@@ -0,0 +1,50 @@
+﻿using IPA;
+using IPA.Logging;
+
+namespace SimplePriority
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class SimplePriority
+    {
+        #region Properties
+        
+        /// <summary>
+        /// The main logger.
+        /// </summary>
+        public static Logger Log { get; private set; }
+        
+        #endregion
+        
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of the logger mod.
+        /// </summary>
+        /// <param name="logger">The logger mod.</param>
+        public SimplePriority(Logger logger)
+        {
+            Log = logger;
+        }
+        
+        #endregion
+        
+        #region Functions
+
+        /// <summary>
+        /// Function triggered when the mod is started.
+        /// </summary>
+        [OnStart]
+        public void OnStart()
+        {
+        }
+        /// <summary>
+        /// Function triggered when the game is exiting.
+        /// </summary>
+        [OnExit]
+        public void OnExit()
+        {
+        }
+        
+        #endregion
+    }
+}
